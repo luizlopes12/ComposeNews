1400956b531ecb36afee1a656b1c37993e6f2283
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-10-26 10:47:04 -0300

    feat: onboarding page

--- app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
@@ -5,11 +5,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.loc.newsapp.ui.theme.WhiteGray
 
 @Composable
 fun NewsButton(
@@ -24,5 +26,20 @@ fun NewsButton(
     shape = RoundedCornerShape(size = 6.dp)
     ){
         Text(text = text, style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold))
+    }
+}
+
+
+@Composable
+fun NewsTextButton(
+    text: String,
+    onClick: () -> Unit
+){
+    TextButton(onClick = onClick){
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = WhiteGray
+        )
     }
 }
